94505e2ccde7f72bf0d312778d16abc839766008
Miguel Osorio <miguelusx@gmail.com>
2025-05-04 16:32:25 -0600

    Exercises 11.10 - 11.12 Fly.io

--- full-stack-open-pokedex/app.js
+++ full-stack-open-pokedex/app.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/version', (_req, res) => {
+  res.send('2.0')
+})
+
+app.get('/health', (_req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
